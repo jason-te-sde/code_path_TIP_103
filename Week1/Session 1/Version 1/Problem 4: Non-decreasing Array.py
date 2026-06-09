5adb22af8e951f7fb8692e01db8e66618a584000
Rigong <te.r@northeastern.edu>
2026-06-08 20:54:24 -0700

    finish problem 6, 7, 8

--- Week1/Session 1/Version 1/Problem 4: Non-decreasing Array.py
+++ Week1/Session 1/Version 1/Problem 4: Non-decreasing Array.py
@@ -17,38 +17,52 @@ Example Output:
 True
 False
 """
-def non_decreasing(nums):
+# def non_decreasing(nums):
 
-    n = len(nums)
-    left, right = 0, n - 1
+#     n = len(nums)
+#     left, right = 0, n - 1
     
-    while left < n - 1 and nums[left] <= nums[left + 1]:
-        left += 1
+#     while left < n - 1 and nums[left] <= nums[left + 1]:
+#         left += 1
     
-    while right > 0 and nums[right] >= nums[right - 1]:
-        right -= 1
+#     while right > 0 and nums[right] >= nums[right - 1]:
+#         right -= 1
     
-    if left >= right:
-        return True
-    elif right - left > 1:
-        return False
-    else:
-        if left == 0 or nums[left - 1] <= nums[right]:
-            return True
-        if right == n - 1 or nums[right + 1] >= nums[left]:
-            return True
+#     if left >= right:
+#         return True
+#     elif right - left > 1:
+#         return False
+#     else:
+#         if left == 0 or nums[left - 1] <= nums[right]:
+#             return True
+#         if right == n - 1 or nums[right + 1] >= nums[left]:
+#             return True
     
-    return False
+#     return False
+def non_decreasing(nums):
+    changed = False
+    for i in range(1, len(nums)):
+        if nums[i] < nums[i - 1]:
+            if changed:
+                return False
+            changed = True
+            if i >= 2 and nums[i] < nums[i - 2]:
+                nums[i] = nums[i - 1]
+            else:
+                nums[i - 1] = nums[i]
+    return True
 
 # Test cases - covers all edge cases
 test_cases = [
-    ([], True),                    # Empty array
-    ([1], True),                   # Single element
-    ([1, 2, 3], True),             # Already sorted
-    ([2, 1, 2], True),             # Conflict at start
-    ([1, 3, 2], True),             # Conflict at end
-    ([3, 4, 2, 3], False),         # Unfixable conflict
-    ([4, 2, 1], False),            # Multiple conflicts
+    ([], True),
+    ([1], True),
+    ([2, 1], True),
+    ([1, 2, 3], True),
+    ([1, 4, 2, 3], True),
+    ([2, 3, 1, 4], True),
+    ([1, 2, 3, 0], True),
+    ([3, 4, 2, 3], False),
+    ([4, 2, 1], False),
 ]
 
 for nums, expected in test_cases:
