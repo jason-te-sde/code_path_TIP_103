be4d6c5fbe2b0447f584042fc76231109ee400f3
Rigong <te.r@northeastern.edu>
2026-06-09 00:30:37 -0700

    version 2 problem 2 is done

---
+++ Week1/Session 1/Version 2/Problem 2: HulkSmash.py
@@ -0,0 +1,46 @@
+"""
+Write a function hulk_smash() that accepts an integer n and returns a 1-indexed string array answer where:
+
+answer[i] == "HulkSmash" if i is divisible by 3 and 5.
+answer[i] == "Hulk" if i is divisible by 3.
+answer[i] == "Smash" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+def hulk_smash(n):
+	pass
+Example Usage:
+
+n = 3
+hulk_smash(n)
+
+n = 5
+hulk_smash(n)
+
+n = 15
+hulk_smash(n)
+Example Output:
+
+["1", "2", "Hulk"]
+["1", "2", "Hulk", "4", "Smash"]
+["1", "2", "Hulk", "4", "Smash", "Hulk", "7", "8", "Hulk", "Smash", "11", "Hulk", "13", "14", "HulkSmash"]
+
+"""
+def hulk_smash(n):
+    res = []
+    for i in range(1, n + 1):
+        text = ""
+        if i % 3 == 0:
+            text += "Hulk"
+        if i % 5 == 0:
+            text += "Smash"
+        res.append(text or str(i))
+    
+    return res
+
+n = 3
+print(hulk_smash(n))
+
+n = 5
+print(hulk_smash(n))
+
+n = 15
+print(hulk_smash(n))
